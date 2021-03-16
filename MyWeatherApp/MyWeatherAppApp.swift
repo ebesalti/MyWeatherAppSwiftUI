b090faf3055de7f208f9d44908b7a4f3c975f5f9
Erkan Besalti <erkan.besalti@gmail.com>
2021-03-16 22:46:36 +0100

    Initial Commit

---
+++ MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by Erkan Besalti on 2021-03-16.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
